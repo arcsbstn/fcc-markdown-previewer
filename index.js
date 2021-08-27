43793e4fd94be1ddc8738f21c62c1c974de615ab
ARC Sebastian <arcsbstn@gmail.com>
2021-08-27 10:32:46 +0800

    add parse + render md; update placeholder text

--- index.js
+++ index.js
@@ -2,7 +2,50 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: "Lorem ipsum..."
+      markdown: `# Welcome to my React Markdown Previewer!
+
+      ## This is a sub-heading...
+      ### And here's some other cool stuff:
+      
+      Heres some code, \`<div></div>\`, between 2 backticks.
+      
+      \`\`\`
+      // this is multi-line code:
+      
+      function anotherExample(firstLine, lastLine) {
+        if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+          return multiLineCode;
+        }
+      }
+      \`\`\`
+      
+      You can also make text **bold**... whoa!
+      Or _italic_.
+      Or... wait for it... **_both!_**
+      And feel free to go crazy ~~crossing stuff out~~.
+      
+      There's also [links](https://www.freecodecamp.org), and
+      > Block Quotes!
+      
+      And if you want to get really crazy, even tables:
+      
+      Wild Header | Crazy Header | Another Header?
+      ------------ | ------------- | -------------
+      Your content can | be here, and it | can be here....
+      And here. | Okay. | I think we get it.
+      
+      - And of course there are lists.
+        - Some are bulleted.
+           - With different indentation levels.
+              - That look like this.
+      
+      
+      1. And there are numbered lists too.
+      1. Use just 1s if you want!
+      1. And last but not least, let's not forget embedded images:
+      
+      ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+      `
     };
   }
 
@@ -18,7 +61,10 @@ class App extends React.Component {
       margin: "0 auto",
       padding: "10px"
     };
-    const previewStyle = editorStyle;
+    const previewStyle = Object.assign(
+      {},
+      editorStyle,
+      { textAlign: "left" });
 
     return (
       <div>
@@ -41,7 +87,11 @@ class App extends React.Component {
             {/*---------- Preview -----------*/}
             <h4><badge>Preview</badge></h4>
             <div id="preview"
-              style={previewStyle}>{this.state.markdown}</div>
+              className="overflow-auto"
+              style={previewStyle}
+              dangerouslySetInnerHTML={{
+                __html: marked(this.state.markdown)
+              }}></div>
           </div>
         </div>
       </div>
